d60f49c8dccd079e6bf1480d4caf73fe6d6b26b6
renjithkumar1 <renjithkumar1@gmail.com>
2019-08-08 10:18:35 +0800

    Changes for refactoring

--- src/components/componentList/componentList.jsx
+++ src/components/componentList/componentList.jsx
@@ -5,7 +5,8 @@ import { connect } from "react-redux";
 // Import Elements
 import TypographyComponent from "../typographyComponent/typographyComponent";
 import TableComponent from "../tableComponent/tableComponent";
-import exportStyles, { getStyles } from "../../cssExports/exportStyles";
+import { getStyles } from "../../cssExports/exportStyles";
+import cssPresets from "../../cssPresets/cssPresets";
 
 
 import { MainNav, Button, Breadcrumb, AccordionBar, Card, Callout, Notification } from "../common";
@@ -49,9 +50,50 @@ class ComponentList extends Component {
   };
 
   // Export style
-  // Export style
   exportStyle = exportcss => {
-    var style = getStyles();
+
+    var variableStart = ":root \r\n { \r\n\t";
+
+    var themePrimaryColor = ""
+    if (cssPresets.themePresets.primaryColor !== this.props.themePrimaryColor) {
+      themePrimaryColor = "--primary-color: " + this.props.themePrimaryColor + "; \r\n\t"
+    }
+
+    var themeSecondaryColor = ""
+    if (cssPresets.themePresets.secondaryColor !== this.props.themeSecondaryColor) {
+      themeSecondaryColor = "--secondary-color: " + this.props.themeSecondaryColor + "; \r\n\t"
+    }
+
+    var infoColor = ""
+    if (cssPresets.themePresets.infoColor !== this.props.infoColor) {
+      infoColor = "--info-color: " + this.props.infoColor + "; \r\n\t"
+    }
+
+    var successColor = ""
+    if (cssPresets.themePresets.successColor !== this.props.successColor) {
+      successColor = "--success-color: " + this.props.successColor + "; \r\n\t"
+    }
+
+    var dangerColor = ""
+    if (cssPresets.themePresets.dangerColor !== this.props.dangerColor) {
+      dangerColor = "--danger-color: " + this.props.dangerColor + "; \r\n\t"
+    }
+
+    var warningColor = ""
+    if (cssPresets.themePresets.warningColor !== this.props.warningColor) {
+      warningColor = "--warning-color: " + this.props.warningColor + "; \r\n"
+    }
+
+    var tableHeadColor = ""
+    if (cssPresets.tablePresets.tableHeadColorDefault !== this.props.tableHeadColor) {
+      tableHeadColor = "table \r\n { \r\n\t background-color: " + this.props.tableHeadColor + "; \r\n } \r\n"
+    }
+
+    var variableEnd = "\r\n } \r\n ";
+
+    var variables = variableStart + themePrimaryColor + themeSecondaryColor + infoColor + successColor + dangerColor + warningColor + tableHeadColor + variableEnd;
+
+    var style = variables + getStyles();
     if (style.length > 0) {
       console.log(style);
       var data = new Blob([style], { type: "text/css" });
